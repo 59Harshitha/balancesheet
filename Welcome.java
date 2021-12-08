e4e230992d07620124400eed49dae33b91c8ee7f
Harshitha <92156731+59Harshitha@users.noreply.github.com>
2021-12-08 14:41:56 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+package af.uki.smoke;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Welcome {
+	
+	@Test 
+
+	public void welCome(){
+		// TODO Auto-generated method stub
+		Reporter.log("Hello world", true);
+	}
+
+}
